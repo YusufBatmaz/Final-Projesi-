ae939b1097a92b680a652b5b7a67dc18cd2ce2e2
YusufBatmaz <128752354+YusufBatmaz@users.noreply.github.com>
2023-06-02 11:02:04 +0300

    Update MaviYaka.py
    Yorum satırları eklendi.

--- MaviYaka.py
+++ MaviYaka.py
@@ -2,16 +2,19 @@ from Calisan import Calisan
 
 
 class MaviYaka(Calisan):
+    # Değişkenlere göre Initializer metot
     def __init__(self, tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas, yipranma_payi):
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk, sektor, tecrube, maas)
         self.__yipranma_payi = yipranma_payi
 
+    #  Gerekli değişkenler için get/set metotları
     def get_yipranma_payi(self):
         return self.__yipranma_payi
 
     def set_yipranma_payi(self, yipranma_payi):
         self.__yipranma_payi = yipranma_payi
 
+    # Çalışanın zam hakkını hesaplayan zam_hakki metodu
     def zam_hakki(self):
         try:
             if self.get_tecrube() < 2:
@@ -25,12 +28,15 @@ class MaviYaka(Calisan):
 
             self.set_yeni_maas(self.get_maas() + ((self.get_maas() / 100) * zam_oran))
 
+            # Yeni maaş, eski maaş ile aynıysa eski maaş, yeni maaşa atanır
             if self.get_yeni_maas() == self.get_maas():
                 self.set_yeni_maas(self.get_maas())
 
         except Exception as e:
             print("Hata:", str(e))
 
+    # ad, soyad, tecrübe ve yeni maaşı bilgilerini yazdıran method
     def __str__(self):
         self.zam_hakki()
-        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nTecrübe: {self.get_tecrube()} ay\nYeni Maaş: {self.get_yeni_maas()}"
+        return f"Ad: {self.get_ad()}\nSoyad: {self.get_soyad()}\nTecrübe: {self.get_tecrube()} ay" \
+               f"\nYeni Maaş: {self.get_yeni_maas()}"
